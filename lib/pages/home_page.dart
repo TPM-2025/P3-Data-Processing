c3c35d0bf38c217495f1dbe51b227eac00567b45
plirapli <rafliyulistiawan@gmail.com>
2025-03-07 08:01:41 +0700

    fix: p3 data processing & navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,40 +1,12 @@
-import 'package:asisten_tpm_3/model/game_model.dart';
-import 'package:asisten_tpm_3/pages/detail_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   final String username;
+
   const HomePage({super.key, required this.username});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Welcome, $username")),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 8,
-        ),
-        itemBuilder: (context, index) => _gameList(context, index),
-        itemCount: gameList.length,
-      ),
-    );
-  }
-
-  Widget _gameList(BuildContext context, int index) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => DetailPage(index: index)),
-        );
-      },
-      child: Column(
-        children: [
-          Image.network(gameList[index].imageUrls[0]),
-          Text(gameList[index].name),
-        ],
-      ),
-    );
+    return Scaffold(appBar: AppBar(title: Text(username)));
   }
 }
